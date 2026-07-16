aaeea7baf60072aa436c942df4951dac355c5a75
Naomi Elizabeth Jurado Casagallo <nomish.jurado@gmail.com>
2026-07-16 09:26:28 -0500

    Verificacion si el programa hace Run - Se encontro el problema

--- Matricula.java
+++ Matricula.java
@@ -20,7 +20,7 @@ void main() {
         String inputNota = readln("Ingrese la nota final obtenida en '" + materialPrerrequisito + "' (0-10): ");
         notaPrerrequisito = Double.parseDouble(inputNota);
     } else {
-        println("-> Registrando como estudiante de Primer Semestre...");
+        println("-> Registrado como estudiante de Primer Semestre...");
     }
 
     println("\n--- MATERIA A SOLICITAR ---");
@@ -30,32 +30,35 @@ void main() {
     if (respuestaCupo.equalsIgnoreCase("S")) {
         boolean cumplePrerrequisito = false;
 
-        if (historialMaterias!= null) {
-        for (int i = 0; i < historialMaterias.length; i++) {
-            if (historialMaterias[i].equals(materialPrerrequisito)) {
-                cumplePrerrequisito = true;
-            }
-        }
-
-        String resultadoMatricula = switch (String.valueOf(cumplePrerrequisito)) {
-            case "true" -> {
-                if (notaPrerrequisito >= 7.0) {
-                    if (notaPrerrequisito == 10.0) {
-                        yield "FELICIDADES ha sido seleccionad@ como tutor de programacion del PUCE TEC.\nMATRICULA APROBADA: cumple con el prerrequisito con notav perfecta!!";
-                    } else {
-                        yield "MATRÍCULA APROBADA: cumple con el prerrequisito con nota" + notaPrerrequisito;
-                    }
-                } else {
-                    yield "MATRICULA RECHAZADA: Reprobo el prerrequisito con " + notaPrerrequisito;
+        if (historialMaterias != null) {
+            for (int i = 0; i < historialMaterias.length; i++) {
+                if (historialMaterias[i].equals(materialPrerrequisito)) {
+                    cumplePrerrequisito = true;
                 }
             }
-            case "false" -> "MATRÍCULA RECHAZADA: No cuenta con el prerrequisito en su historial.";
-            default -> "Error del sistema.";
-        };
 
-        println("\n[RESULTADO]: " + resultadoMatricula);
+            String resultadoMatricula = switch (String.valueOf(cumplePrerrequisito)) {
+                case "true" -> {
+                    if (notaPrerrequisito >= 7.0) {
+                        if (notaPrerrequisito == 10.0) {
+                            yield "FELICIDADES ha sido seleccionad@ como tutor de programacion del PUCE TEC.\nMATRICULA APROBADA: cumple con el prerrequisito con notav perfecta!!";
+                        } else {
+                            yield "MATRÍCULA APROBADA: cumple con el prerrequisito con nota" + notaPrerrequisito;
+                        }
+                    } else {
+                        yield "MATRICULA RECHAZADA: Reprobo el prerrequisito con " + notaPrerrequisito;
+                    }
+                }
+                case "false" -> "MATRÍCULA RECHAZADA: No cuenta con el prerrequisito en su historial.";
+                default -> "Error del sistema.";
+            };
 
-    } else {
-        println("\nProceso finalizado. No se solicitaron materias de segundo nivel.");
+            println("\n[RESULTADO]: " + resultadoMatricula);
+
+        } else {
+            println("\nProceso finalizado. No se solicitaron materias de segundo nivel.");
+        }
     }
-}
+
+    // NOTA: No Corre el programa debido a un problema de la estructuracion del archivo java. No esta reconociendo las carateristicas experimentales.
+
---
+++ out/production/PUCEFundamentos01/README.md
@@ -0,0 +1,59 @@
+# PUCEFundamentos01
+## 🚀 Taller Autónomo - Operación Código Legacy
+
+**Integrantes:**
+*Naomi Jurado
+*Odalis Bonilla
+
+# FASE 1 INGENIERIA INVERSA - EXPLICACIÓN
+
+## 1. ¿Qué problema resuelve esta aplicación?
+
+Es un simulador de matriculas particularmente de la Tecnologia en Desarrollo De software.
+Esta aplicación tiene como objetivo evaluar si un estudiante puede matricularse o no en la materia 'Estructura de datos', basandose en la verificación del requisito previo (es obligatorio hacer cursado y aprobado la materia 'Fundamentos de Programación' con una nota mínima de 7.0) para acceder a esta materia. 
+
+## 2. Descripción del flujo de datos
+
+A continuación se describe el como opera el sistema de matrícula.
+
+### Ingreso de Datos Iniciales (Inputs)
+El programa solicita:
+  *Nombre del estudiante ('urs')*
+  *¿Es estudiante de reingreso o arrastre? ('s/n')*
+
+ ### Historial de Respuestas
+ *- Si el estudiante responde Sí ('S'):* el programa asume que ya se ha cursado materias anteriormente.
+ Lo que resgistra en su historial ('h_materias') la materia de 'Fundamentos de Programación'.
+ Inmediatamente, imprime un mensaje donde solicita ingresar la nota final obtenida de la materia ('n1'); la nota se convierte en un valor numerio decimal ('double').
+
+ *- Si el estudiante responde No ('N'):* el programa asume que el estudiante es de nuevo ingreso, de primer sementre, y se salta el input de la nota final.
+
+### Solicitud de Cupo
+El programa pregunta: 
+ *¿Desea solicitar el cupo para esta materia? ('s/n')*
+ ### Historial de Respuestas
+ *- Si el estudiante responde Sí ('S'):* el programa evalua si cumple con los requisitos para proporcionar el cupo.
+
+ *- Si el estudiante responde No ('N'):* el programa imprime un mensaje de despedida: "Proceso finalizado. No se solicitaron materias de segundo nivel" y por último, se cierra el sistema de matrícula.
+
+## 3. Resumen 
+El sistema evalua tres posibles resultados o escenarios: 
+
+  *Matrícula Aprobada:* Si el estudiante es de reingreso ('S') y registra una nota >= a 7.0 en "Fundamentos de Programación", cumple con el prerrequisito.
+
+  *Matrícula Rechazada por nota:* Si es de reingreso ('S') pero su nota < a 7.0, el sistema rechaza la solicitud por no alcanzar el puntaje mínimo.
+
+  *Matrícula Rechazada por falta de prerrequisito:* Si el estudiante es de nuevo ingreso ('N'), no tendrá la materia en su historial, lo que resulta con el rechazo de la matrícula por no contar con el prerrequisito.
+
+  *Nota de cierre:* Si el estudiante decide no solicitar el cupo (respondiendo "N"), el sistema detiene el proceso de inmediato con el mensaje: "Proceso finalizado. No se solicitaron materias de segundo nivel."
+
+ 
+
+
+
+
+
+
+
+
+
